bc8d0114190e5ae954bdf908aacb6a399073431e
toyosge <toyosge@gmail.com>
2016-02-27 18:31:02 +0900

    Used CommandLineRunner

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,9 +1,11 @@
 package com.example;
 
-import com.example.app.Frontend;
+import com.example.app.Argument;
+import com.example.app.ArgumentResolver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -11,11 +13,21 @@ import org.springframework.context.annotation.ComponentScan;
  */
 @EnableAutoConfiguration
 @ComponentScan
-public class App {
+public class App implements CommandLineRunner {
+    @Autowired
+    ArgumentResolver argumentResolver;
+    @Autowired
+    Calculator calculator;
+
+    @Override
+    public void run(String... strings) throws Exception {
+        System.out.println("Enter 2 numbers like 'a,b'");
+        Argument argument = argumentResolver.resolve(System.in);
+        int result = calculator.calc(argument.getA(), argument.getB());
+        System.out.println("result = " + result);
+    }
+
     public static void main(String[] args) {
-        try (ConfigurableApplicationContext context = SpringApplication.run(App.class, args)) {
-            Frontend frontend = context.getBean(Frontend.class);
-            frontend.run();
-        }
+        SpringApplication.run(App.class, args);
     }
 }
