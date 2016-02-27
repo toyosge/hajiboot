e7d003d8faf5dbe6e07f26613a81483527ac19da
toyosge <toyosge@gmail.com>
2016-02-27 21:54:56 +0900

    Used JDBC

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,12 +1,13 @@
 package com.example;
 
-import com.example.domain.Customer;
-import com.example.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 
 /**
  * Hello world!
@@ -15,17 +16,14 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan
 public class App implements CommandLineRunner {
     @Autowired
-    CustomerService customerService;
+    NamedParameterJdbcTemplate jdbcTemplate;
 
     @Override
     public void run(String... strings) throws Exception {
-        // データ追加
-        customerService.save(new Customer(1, "Nobita", "Nobi"));
-        customerService.save(new Customer(2, "Takeshi", "Goda"));
-        customerService.save(new Customer(3, "Suneo", "Honekawa"));
-
-        // データ表示
-        customerService.findAll().forEach(System.out::println);
+        String sql = "SELECT 1";
+        SqlParameterSource param = new MapSqlParameterSource();
+        Integer result = jdbcTemplate.queryForObject(sql, param, Integer.class);
+        System.out.println("result = " + result);
     }
 
     public static void main(String[] args) {
