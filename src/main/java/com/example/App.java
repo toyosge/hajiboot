b9c1833b091d90d2c8098be67d3dab2f0e44fefc
toyosge <toyosge@gmail.com>
2016-02-27 18:43:00 +0900

    Revert to "hello world"

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,10 +1,5 @@
 package com.example;
 
-import com.example.app.Argument;
-import com.example.app.ArgumentResolver;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
@@ -13,21 +8,8 @@ import org.springframework.context.annotation.ComponentScan;
  */
 @EnableAutoConfiguration
 @ComponentScan
-public class App implements CommandLineRunner {
-    @Autowired
-    ArgumentResolver argumentResolver;
-    @Autowired
-    Calculator calculator;
-
-    @Override
-    public void run(String... strings) throws Exception {
-        System.out.println("Enter 2 numbers like 'a,b'");
-        Argument argument = argumentResolver.resolve(System.in);
-        int result = calculator.calc(argument.getA(), argument.getB());
-        System.out.println("result = " + result);
-    }
-
+public class App {
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+        System.out.println("hello world ");
     }
 }
--- src/main/java/com/example/Calculator.java
+++
@@ -1,8 +0,0 @@
-package com.example;
-
-/**
- * Created by masahirayamamoto on 2/27/16.
- */
-public interface Calculator {
-    int calc(int a, int b);
-}
