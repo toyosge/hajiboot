dac7a912d1aee951f3f17bd8a3e75bf8bccaac65
toyosge <toyosge@gmail.com>
2016-02-27 22:30:30 +0900

    Used lamda

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -6,13 +6,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 /**
  * Hello world!
@@ -27,12 +23,8 @@ public class App implements CommandLineRunner {
     public void run(String... strings) throws Exception {
         String sql = "SELECT id, first_name, last_name FROM customers WHERE id = :id";
         SqlParameterSource param = new MapSqlParameterSource().addValue("id", 1);
-        Customer result = jdbcTemplate.queryForObject(sql, param, new RowMapper<Customer>() {
-            @Override
-            public Customer mapRow(ResultSet rs, int rowNum) throws SQLException {
-                return new Customer(rs.getInt("id"), rs.getString("first_name"), rs.getString("last_name"));
-            }
-        });
+        Customer result = jdbcTemplate.queryForObject(sql, param, (rs, rowNow) ->
+                new Customer(rs.getInt("id"), rs.getString("first_name"), rs.getString("last_name")));
         System.out.println("result = " + result);
     }
 
