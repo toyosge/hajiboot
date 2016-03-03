6b219044af4bf85ddb62bb3026c9847cb985bdac
toyosge <toyosge@gmail.com>
2016-03-03 21:36:14 +0900

    Updated repository class using "Spring Data JPA"

--- src/main/java/com/example/domain/Customer.java
+++ src/main/java/com/example/domain/Customer.java
@@ -2,14 +2,25 @@ package com.example.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 
 /**
  * Created by masahirayamamoto on 2/27/16.
  */
+@Entity
+@Table(name = "customers")
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class Customer {
+    @Id
+    @GeneratedValue
     private Integer id;
+    @Column(nullable = false)
     private String firstName;
+    @Column(nullable = false)
     private String lastName;
+
 }
--- src/main/java/com/example/repository/CustomerRepository.java
+++ src/main/java/com/example/repository/CustomerRepository.java
@@ -1,71 +1,11 @@
 package com.example.repository;
 
 import com.example.domain.Customer;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.core.namedparam.SqlParameterSource;
-import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
-
-import javax.annotation.PostConstruct;
-import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  * Created by masahirayamamoto on 2/27/16.
  */
-@Repository
-@Transactional
-public class CustomerRepository {
-    @Autowired
-    NamedParameterJdbcTemplate jdbcTemplate;
 
-    SimpleJdbcInsert insert;
-
-    @PostConstruct
-    public void init() {
-        insert = new SimpleJdbcInsert((JdbcTemplate) jdbcTemplate.getJdbcOperations())
-                .withTableName("customers")
-                .usingGeneratedKeyColumns("id");
-    }
-
-    private static final RowMapper<Customer> customerROWMapper = (rs, i) -> {
-        Integer id = rs.getInt("id");
-        String firstName = rs.getString("first_name");
-        String lastName = rs.getString("last_name");
-        return new Customer(id, firstName, lastName);
-    };
-
-    public List<Customer> findAll() {
-        List<Customer> customers = jdbcTemplate.query(
-                "SELECT id,first_name,last_name FROM customers ORDER BY id", customerROWMapper);
-        return customers;
-    }
-
-    public Customer findOne(Integer id) {
-        SqlParameterSource param = new MapSqlParameterSource().addValue("id", id);
-        return jdbcTemplate.queryForObject("SELECT id,first_name,last_name FROM customers WHERE if=:id", param, customerROWMapper);
-    }
-
-    public Customer save(Customer customer) {
-        SqlParameterSource param = new BeanPropertySqlParameterSource(customer);
-        if (customer.getId() == null) {
-            Number key = insert.executeAndReturnKey(param);
-            customer.setId(key.intValue());
-            jdbcTemplate.update("INSERT INTO customers(first_name,last_name) values(:firstName, :lastName)", param);
-        } else {
-            jdbcTemplate.update("UPDATE customers SET first_name=:firstName, last_name=:lastName WHERE id=:id", param);
-        }
-        return customer;
-    }
-
-
-    public void delete(Integer id) {
-        SqlParameterSource param = new MapSqlParameterSource().addValue("id", id);
-        jdbcTemplate.update("DELETE FROM customers WHERE id=:id", param);
-    }
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
 }
