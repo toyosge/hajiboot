1c6bdcde6f488fb6ed3bc484b467d50869fa96d6
toyosge <toyosge@gmail.com>
2016-03-03 22:05:10 +0900

    Updated repository class defining query using "JPQL"

--- src/main/java/com/example/repository/CustomerRepository.java
+++ src/main/java/com/example/repository/CustomerRepository.java
@@ -2,10 +2,15 @@ package com.example.repository;
 
 import com.example.domain.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 /**
  * Created by masahirayamamoto on 2/27/16.
  */
 
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+    @Query("SELECT x FROM Customer x ORDER BY x.firstName, x.lastName")
+    List<Customer> findAllOrderByName();
 }
