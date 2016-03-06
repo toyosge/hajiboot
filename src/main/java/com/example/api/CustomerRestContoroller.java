0003c1f7208b922fb7a3270854f68a1f6612ea6e
toyosge <toyosge@gmail.com>
2016-03-06 15:15:27 +0900

    Added CREATE, UPDATE, DELETE method

--- src/main/java/com/example/api/CustomerRestContoroller.java
+++ src/main/java/com/example/api/CustomerRestContoroller.java
@@ -3,10 +3,8 @@ package com.example.api;
 import com.example.domain.Customer;
 import com.example.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,5 +27,23 @@ public class CustomerRestContoroller {
     Customer getCustomer(@PathVariable Integer id) {
         Customer customer = customerService.findOne(id);
         return customer;
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
+    Customer postCustomers(@RequestBody Customer customer) {
+        return customerService.create(customer);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+    Customer putCustomer(@PathVariable Integer id, @RequestBody Customer customer) {
+        customer.setId(id);
+        return customerService.update(customer);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    void deleteCustomer(@PathVariable Integer id) {
+        customerService.delete(id);
     }
 }
